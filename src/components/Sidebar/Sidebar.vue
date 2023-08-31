5279a2b1ff7fc11e9d7c48dabce85b535e1016b3
Ezraliss800 <80759979+Ezraliss800@users.noreply.github.com>
2023-08-31 11:52:52 +0100

    Update Sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -2,6 +2,13 @@
   <v-navigation-drawer>
     <v-list>
       <v-list-item title="Timetables4Us"></v-list-item>
+      <v-list-item>
+        <v-btn block @click="$router.push('/')">Home </v-btn>
+      </v-list-item>
+
+      <v-list-item>
+        <v-btn block @click="$router.push('/login')">Login</v-btn>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
